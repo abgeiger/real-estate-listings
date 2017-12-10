878397306b5fb2db4ae6ef20a5bf90e5b99fd50a
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-12-10 13:15:35 -0600

    rename schemas and routes

--- server/public/scripts/services/forRent.service.js
+++ server/public/scripts/services/forRent.service.js
@@ -1,22 +1,22 @@
 app.service('ForRentService', ['$http', function($http) {
     var self = this;
 
-    // self.powers = { list: [] };
+    self.rentals = { list: [] };
     // self.newPower = {
     //     name: '',
     //     description: ''
     // }
 
-    // self.getPowers = function() {
-    //     $http({
-    //         method: 'GET',
-    //         url: '/power'
-    //     }).then(function(response) {
-    //         console.log('response', response);
+    self.getRentals = function() {
+        $http({
+            method: 'GET',
+            url: '/forRent'
+        }).then(function(response) {
+            console.log('response', response);
             
-    //         self.powers.list = response.data;
-    //     });
-    // };
+            self.powers.list = response.data;
+        });
+    };
 
     // self.addNewPower = function(newPower) {
     //     $http({
