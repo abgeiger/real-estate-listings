eb1ab30752af32df65e97745bffa43ac77a8558c
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-12-10 13:15:15 -0600

    rename schemas and routes

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var mongooseConnection = require('./modules/mongoose-connection');
-var forRent = require('./routes/forRent.js');
-var forSale = require('./routes/forSale.js');
+var rentals = require('./routes/rentals.js');
+var listings = require('./routes/listings.js');
 
 var app = express();
 
@@ -15,8 +15,8 @@ app.use(express.static('server/public'));
 // database connection
 mongooseConnection.connect();
 
-app.use('/forRent', forRent);
-app.use('/forSale', forSale);
+app.use('/rentals', rentals);
+app.use('/listings', listings);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function(){
