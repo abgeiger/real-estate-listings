52c653fc79e3883e8d56731b4ea7a8049e93de1c
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-12-10 13:26:58 -0600

    Revert "rename schemas and routes"
    This reverts commit eb1ab30752af32df65e97745bffa43ac77a8558c.

---
+++ server/routes/forSale.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var ForSale = require('../models/forSale.schema');
+
+// var Movie = require('../models/movie.schema');
+
+// router.get('/', function(req, res){
+//     // a get request for all games
+//     Movie.find({}, function(errorMakingDatabaseQuery, data){
+//         if (errorMakingDatabaseQuery) {
+//             console.log('error with game find', errorMakingDatabaseQuery);
+//             res.sendStatus(500);
+//         } else {
+//             res.send(data);
+//         }
+//     });
+// });
+
+// router.post('/', function(req, res){
+//     // a get request for all games
+//     var addMovie = new Movie(req.body);
+//     addMovie.save(function(errorMakingDatabaseQuery, data){
+//         if (errorMakingDatabaseQuery) {
+//             console.log('error with game find', errorMakingDatabaseQuery);
+//             res.sendStatus(500);
+//         } else {
+//             res.sendStatus(200);
+//         }
+//     });
+// });
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var mongooseConnection = require('./modules/mongoose-connection');
-var rentals = require('./routes/rentals.js');
-var listings = require('./routes/listings.js');
+var forRent = require('./routes/forRent.js');
+var forSale = require('./routes/forSale.js');
 
 var app = express();
 
@@ -15,8 +15,8 @@ app.use(express.static('server/public'));
 // database connection
 mongooseConnection.connect();
 
-app.use('/rentals', rentals);
-app.use('/listings', listings);
+app.use('/forRent', forRent);
+app.use('/forSale', forSale);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function(){
